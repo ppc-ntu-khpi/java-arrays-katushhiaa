962e5192c3d7b2ceffdb65dd45d58a2a670deae9
katushhiaa <113555695+katushhiaa@users.noreply.github.com>
2023-02-13 19:20:40 +0200

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,8 +5,8 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int[] array = new int[8];
+        int n = array.length;
+        Exercise.Calculate(array, n);
     }
 }
